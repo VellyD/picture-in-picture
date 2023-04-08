f176b052056c8f98b15b9b5210d5cce4cc31c7cf
VellyD <viddvd3@gmail.com>
2023-04-08 21:04:07 +0300

    adding the media stream functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+async function selectMedia() {
+  try {
+    const mediaSteam = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaSteam;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (err) {
+    alert("Error fount:", err);
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disable = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+selectMedia();
